7aaedfff27650e304f4de2fbed297c13068d965b
Martin Kapal <flamecze@gmail.com>
2019-10-05 19:28:47 +0200

    Svelte migration WIP

---
+++ src/Timestamp.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let timestamp = undefined;
+  const dateOptions = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit',
+  };
+
+  $: formattedTimestamp =
+    timestamp !== undefined
+      ? new Date(timestamp).toLocaleDateString('cs', dateOptions)
+      : '-';
+</script>
+
+<style>
+  div {
+    font-size: 20px;
+  }
+</style>
+
+<div>
+  {formattedTimestamp}
+</div>
