ed71a422dacab6164ba5d6cf76ac60e1491e0c22
Martin Kapal <flamecze@gmail.com>
2019-10-06 02:41:28 +0200

    Add a line graph with temperature history

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,15 @@
 <script>
   import Sockette from 'sockette';
+
   import Temperature from './Temperature.svelte';
   import Timestamp from './Timestamp.svelte';
+  import LineGraph from './LineGraph.svelte';
 
   let connecting = true;
   let error = false;
   let temperature;
   let timestamp;
+  let historyData = [{ x: 0, y: 0 }];
 
   new Sockette(process.env.WEBSOCKET_SERVER, {
     timeout: 5000,
@@ -20,6 +23,14 @@
       const data = JSON.parse(e.data);
       temperature = data.temperature;
       timestamp = data.timestamp;
+
+      if (data.latestTemperatures && data.latestTemperatures.length > 0) {
+        historyData = data.latestTemperatures.map((val, idx) => ({
+          x: idx + 1,
+          y: val,
+        }));
+      }
+
       console.log('Message received', data);
     },
     onreconnect: () => {
@@ -46,11 +57,11 @@
   .appContainer {
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-evenly;
     align-items: center;
     height: 100%;
     background-color: #3f8eb6;
-    color: white;
+    color: #fff;
   }
 </style>
 
@@ -61,8 +72,14 @@
     <img src="spinner.svg" alt="Loading" />
     {connecting ? 'Připojování' : 'Čekání'}
   {:else}
-    <Temperature {temperature} />
-    <Timestamp {timestamp} />
+    <div>
+      <Temperature {temperature} />
+      <Timestamp {timestamp} />
+    </div>
+    <LineGraph
+      points={historyData}
+      yTicks={[-20, 0, 20, 40]}
+      xTicks={[1, 25, 50, 75, 100]} />
   {/if}
 
 </div>
