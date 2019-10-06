a4a3f9754f87ceaefa33b0022cea469a6256e1e0
Martin Kapal <flamecze@gmail.com>
2019-10-06 14:09:15 +0200

    Change app background

--- src/App.svelte
+++ src/App.svelte
@@ -61,7 +61,26 @@
     align-items: center;
     align-content: center;
     height: 100%;
-    background-color: #3f8eb6;
+    background: #1e5799; /* Old browsers */
+    background: -moz-linear-gradient(
+      top,
+      #1e5799 0%,
+      #2989d8 50%,
+      #7db9e8 100%
+    ); /* FF3.6-15 */
+    background: -webkit-linear-gradient(
+      top,
+      #1e5799 0%,
+      #2989d8 50%,
+      #7db9e8 100%
+    ); /* Chrome10-25,Safari5.1-6 */
+    background: linear-gradient(
+      to bottom,
+      #1e5799 0%,
+      #2989d8 50%,
+      #7db9e8 100%
+    ); /* W3C, IE10+, FF16+, Chrome26+, Opera12+, Safari7+ */
+    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#1e5799', endColorstr='#7db9e8',GradientType=0 ); /* IE6-9 */
     color: #fff;
   }
 </style>
