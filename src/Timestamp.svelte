ec4cf81b5c0a061905d24e4e1624e1c2cb315abb
Martin Kapal <flamecze@gmail.com>
2019-10-06 13:23:25 +0200

    Use OpenSans font

--- src/Timestamp.svelte
+++ src/Timestamp.svelte
@@ -1,17 +1,11 @@
 <script>
   export let timestamp = undefined;
   const dateOptions = {
-    year: 'numeric',
     month: 'long',
     day: 'numeric',
     hour: '2-digit',
     minute: '2-digit',
   };
-
-  $: formattedTimestamp =
-    timestamp !== undefined
-      ? new Date(timestamp).toLocaleDateString('cs', dateOptions)
-      : '-';
 </script>
 
-<div>{formattedTimestamp}</div>
+<div>{timestamp !== undefined ? new Date(timestamp).toLocaleDateString(undefined, dateOptions) : ''}</div>
