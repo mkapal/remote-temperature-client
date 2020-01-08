684b42009c1bc3584c7d0817e4c22f300db37a59
Martin Kapal <flamecze@gmail.com>
2020-01-08 19:57:07 +0100

    Simple temperature output

--- app.js
+++ app.js
@@ -1,8 +1,40 @@
 import { h, render } from 'https://unpkg.com/preact@latest?module';
-// import { useState } from 'https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module';
+import { useEffect, useState } from 'https://unpkg.com/preact@latest/hooks/dist/hooks.module.js?module';
+import Sockette from 'https://unpkg.com/sockette@latest?module';
 
 const App = () => {
-  return h('div', { class: 'Dashboard' }, 'Hello world!');
+  const [temperature, setTemperature] = useState(undefined);
+
+  useEffect(() => {
+    new Sockette('wss://p.lempls.com', {
+      timeout: 1000,
+      maxAttempts: 10,
+      onopen: () => {
+        console.info('Connected');
+      },
+      onmessage: event => {
+        const data = JSON.parse(event.data);
+
+        if (data.hasOwnProperty('temperature')) {
+          setTemperature(data.temperature);
+        }
+      },
+      onreconnect: () => {
+        console.info('Reconnecting...');
+      },
+      onmaximum: () => {
+        console.warn('Stop attempting');
+      },
+      onclose: () => {
+        console.warn('Closed');
+      },
+      onerror: () => {
+        console.error('Connection error');
+      },
+    });
+  }, []);
+
+  return h('p', {}, temperature);
 };
 
 render(
