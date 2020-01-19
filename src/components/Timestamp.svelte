4b45848827694237d9ea4a4034cfe604eae77edd
Martin Kapal <flamecze@gmail.com>
2020-01-19 11:03:15 +0100

    Fade date when loading

--- src/components/Timestamp.svelte
+++ src/components/Timestamp.svelte
@@ -1,5 +1,6 @@
 <script>
   import { fade } from 'svelte/transition';
+  export let connecting = true;
   export let timestamp = undefined;
   export let locale = undefined;
   const dateOptions = {
@@ -10,6 +11,12 @@
   };
 </script>
 
-<div in:fade>
+<style>
+  .faded {
+    opacity: 0.5;
+  }
+</style>
+
+<div class="{connecting ? 'faded' : ''}" in:fade>
   {timestamp !== undefined ? new Date(timestamp).toLocaleDateString(locale, dateOptions) : ''}
 </div>
