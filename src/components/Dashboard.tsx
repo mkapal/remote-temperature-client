6b23201f6f988d7e2508e9d17ddd3ea3a0c6d522
Martin Kapal <flamecze@gmail.com>
2020-05-19 22:56:56 +0200

    Add a temperature gradient

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,12 +1,40 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Dashboard = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-evenly;
-  align-items: center;
-  align-content: center;
-  height: 100%;
-  background: linear-gradient(to bottom, #1e5799 0%, #2989d8 50%, #7db9e8 100%);
-  color: #fff;
-`;
+type Props = {
+  temperature?: number;
+};
+
+const getHue = (temperature: number) => {
+  const minTemperature = -20;
+  const maxTemperature = 40;
+
+  const minHue = 270;
+  const maxHue = 0;
+
+  return (
+    maxHue +
+    ((minHue - maxHue) * (maxTemperature - temperature)) /
+      (maxTemperature - minTemperature)
+  );
+};
+
+export const Dashboard = styled.div<Props>(({ temperature }) => {
+  const hue = temperature !== undefined ? getHue(temperature) : 210;
+
+  return css`
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    align-content: center;
+    width: 100%;
+    height: 100%;
+    background: linear-gradient(
+      to bottom,
+      hsl(${hue}, 67%, 30%) 0%,
+      hsl(${hue}, 69%, 45%) 50%,
+      hsl(${hue}, 70%, 62%) 100%
+    );
+    color: #fff;
+  `;
+});
