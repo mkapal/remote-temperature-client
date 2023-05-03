9ae89117106748e116f73dd4908ce128736db7aa
Martin Kapal <flamecze@gmail.com>
2023-05-03 20:25:50 +0200

    Upgrade dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(<App />);
 
 serviceWorker.register();
